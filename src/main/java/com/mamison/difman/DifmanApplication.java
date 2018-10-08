dbbecabd4732dec7c5c95561840567ea40904e90
geonyoungkim <c2619zz@naver.com>
2018-10-08 20:03:47 +0900

    jpa batch congif 생성

--- src/main/java/com/mamison/difman/DifmanApplication.java
+++ src/main/java/com/mamison/difman/DifmanApplication.java
@@ -3,10 +3,12 @@ package com.mamison.difman;
 import com.mamison.difman.entity.Account;
 import com.mamison.difman.entity.Report;
 import com.mamison.difman.repository.ReportRepository;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableBatchProcessing
 @SpringBootApplication
 public class DifmanApplication {
 
